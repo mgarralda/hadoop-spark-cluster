7c8b2151dfbaf5f7f5ecb6b591421d29a51acd71
Mariano Garralda <mgarralda68@gmail.com>
2022-08-31 20:27:33 +0200

    Delete open_webuis.ps1

--- open_webuis.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Opening YARN WebUI..."
-start http://localhost:8088 
-
-Write-Host "Opening Spark History WebUI..."
-start http://localhost:18080
-
-Write-Host "Opening HDFS NameNode..."
-start http://localhost:50070
